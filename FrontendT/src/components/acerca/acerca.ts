f64df0f44343b2fe0629220c29c0cfb182452c49
luisPosadas3 <22690351@tecvalles.mx>
2025-10-05 20:10:45 -0600

    Eliminar Carpeta

--- FrontendT/src/components/acerca/acerca.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-acerca',
-  standalone: true,
-  templateUrl: './acerca.html',
-  styleUrls: ['./acerca.css'] 
-})
-export class Acerca {}
